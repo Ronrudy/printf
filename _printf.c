9694e385147f96b0307ed7880380f831d299a1e9
Diana Sefakor Acquah <dianaacquah8@gmail.com>
2023-08-21 09:49:01 -0700

    printf

---
+++ _printf.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * _printf - printf function
+ * @format: format string
+ * Return: the number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	int count = 0;
+	va_list args;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+
+			if (*format == '\0') return (-1);
+			
+			if (*format == 'c')
+			{
+				count += _putchar(va_arg(args, int));
+			}
+			else if (*format == 's')
+			{
+				const char *str = va_arg(args, const char *);
+
+				if (!str) str = "(null)";
+
+				while (*str) count += _putchar(*str++);
+			}
+			else if (*format == '%')
+			{
+				count += _putchar('%');
+			}
+		}
+		else
+		{
+			count += _putchar(*format);
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
+}
