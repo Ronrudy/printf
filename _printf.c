cfcc3215a07df90f00a72ed73f795673005b4322
Ronrudy <Ruffroony@yahoo.com>
2023-08-22 07:02:51 -0700

    i taya

--- _printf.c
+++ _printf.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <string.h>
+
 /**
  * _printf - printf function
  * @format: format string
@@ -9,9 +9,9 @@ int _printf(const char *format, ...)
 {int count = 0;
 	va_list args;
 
+	va_start(args, format);
 	if (format == NULL)
 		return (-1);
-	va_start(args, format);
 
 	while (*format)
 	{
